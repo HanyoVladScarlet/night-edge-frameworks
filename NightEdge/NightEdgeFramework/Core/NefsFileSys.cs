794370e3c2c0d119d7187be206e027770b9a0b3e
Vladimir_Lyapunov <47584292+HanyoVladScarlet@users.noreply.github.com>
2021-01-22 17:24:14 +0800

    Delete binary files and realize
    Delete binary files and realize first version of file split system.

---
+++ NightEdge/NightEdgeFramework/Core/NefsFileSys.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace NightEdgeFramework.Core
+{
+    public class NefsFileSys
+    {
+        private static NefsFileSys fileSys;
+
+        // 用于临时缓存的文件夹目录
+        private string sendBufferDirectory;
+        private string recvBufferDirectory;
+
+        #region Initialize
+
+        private NefsFileSys()
+        {
+
+        }
+
+        public static NefsFileSys GetNefsFileSys()
+        {
+            if (fileSys == null)
+            {
+                fileSys = new NefsFileSys();
+            }
+
+            return fileSys;
+        }
+
+        #endregion
+
+
+        #region Packup for transfer
+
+        public void PackFiles(string targetName,string targetPath,string tempFilePath,int per_size)
+        {
+            NefsFilePacker packer = new NefsFilePacker { TempFilePath = tempFilePath, TargetDirectory = targetPath, UnitSize = per_size ,TargetName = targetName};
+            packer.PackUp();
+        }
+
+        public void UnpackFiles(string targetName,string targetPath, string tempFilePath, int per_size)
+        {
+            NefsFilePacker packer = new NefsFilePacker { TempFilePath = tempFilePath, TargetDirectory = targetPath, UnitSize = per_size,TargetName=targetName };
+            packer.Unpack();
+        }
+
+        #endregion
+
+        public void ReadFile(Uri uri, ref string content)
+        {
+            string path = uri.ToString();
+            using (FileStream fs = new FileStream(path,FileMode.OpenOrCreate))
+            {
+                byte[] buffer = new byte[1024];
+                fs.Read(buffer, 0, 1024);
+                content = Encoding.UTF8.GetString(buffer);
+            }
+        }
+
+        public void WriteFile(string path,string content)
+        {
+            string filePath = path + @"\temp.txt";
+            if (!File.Exists(filePath))
+            {
+                File.Create(path + @"\temp.txt");
+                File.SetAttributes(filePath,
+                    (new FileInfo(filePath)).Attributes | FileAttributes.Normal);
+            }
+
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                using(StreamWriter sr = new StreamWriter(fs))
+                {
+                    sr.WriteLine(content);
+                }
+                
+            }
+        }
+
+    }
+
+    public class NefsFilePacker
+    {
+        public string TempFilePath { get; set; }
+        public string TargetDirectory { get; set; }
+        public int UnitSize { get; set; }
+        public string TargetName { get; set; }
+        public string TargetPath { get { return this.TargetDirectory + @"\" + TargetName; } }
+
+        // 将目标文件打包
+        public void PackUp()
+        {
+            if (!Directory.Exists(this.TempFilePath))
+                Directory.CreateDirectory(this.TempFilePath);
+
+            using(var fsRead = new FileStream(this.TargetDirectory+ @"\" + TargetName, FileMode.Open))
+            {
+                long targetLength = fsRead.Length;
+                int count = 0;
+                //int start = 0;
+
+                Action action = () => { 
+                    using (var fsWrite = new FileStream(TempFilePath+@"\"+count.ToString()+".nefstmp", FileMode.Create))
+                    {
+                        byte[] buffer = new byte[this.UnitSize];
+                        int len = fsRead.Read(buffer, 0, this.UnitSize);
+
+                        fsWrite.Write(buffer, 0, len);
+
+                        Console.WriteLine(Encoding.ASCII.GetString(buffer) + len.ToString());
+                    }
+                    targetLength -= this.UnitSize;
+                };
+
+                while (targetLength > 0)
+                {
+                    action.Invoke();
+                    count++;
+                }
+                    
+            }
+        }
+
+        public void Unpack()
+        {
+            try
+            {
+                if (!Directory.Exists(TargetDirectory))
+                    Directory.CreateDirectory(TargetDirectory);
+                if (!File.Exists(this.TargetPath))
+                    File.Create(this.TargetPath);
+
+                using (var fsWrite = new FileStream(this.TargetPath, FileMode.Create))
+                {
+                    string[] paths = Directory.GetFiles(this.TempFilePath);
+                    int start = 0;
+
+                    foreach (var item in paths)
+                    {
+                        using (var fsReader = new FileStream(item, FileMode.Open))
+                        {
+                            fsWrite.Position = start;
+                            byte[] buffer = new byte[this.UnitSize];
+                            int len = fsReader.Read(buffer, 0, this.UnitSize);
+                            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+                            fsWrite.Write(buffer, 0, len);
+                            start += UnitSize;
+                        }                        
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // 在完成传输后将临时文件施放
+        public void DisposeTempFiles()
+        {
+
+        }
+    }
+}
--- NightEdge/NightEdgeFramework/Core/NightEdgeCore.cs
+++ NightEdge/NightEdgeFramework/Core/NightEdgeCore.cs
@@ -4,8 +4,71 @@ using System.Text;
 
 namespace NightEdgeFramework.Core
 {
+    public class Nyx
+    {
+        private static Nyx nyx;
+        private NightEdgeCore nec;
+
+        #region Initialize
+
+        private Nyx()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            nec = NightEdgeCore.GetNightEdgeCore();
+        }
+
+        public static Nyx GetNyx()
+        {
+            if (nyx == null)
+            {
+                nyx = new Nyx();
+            }
+            return nyx;
+        }
+
+        #endregion
+
+        public NightEdgeCore GetNightEdgeCore()
+        {
+            return nyx.nec;
+        }
+
+        public NefsFileSys GetNefsFileSys()
+        {
+            return nec.GetNefsFileSys();
+        }
+    }
+
     public class NightEdgeCore
     {
+        private static NightEdgeCore core;
+        private NefsFileSys fileSys; 
 
+        private NightEdgeCore()
+        {
+
+        }
+
+        public static NightEdgeCore GetNightEdgeCore()
+        {
+            if(core == null)
+            {
+                core = new NightEdgeCore();
+            }
+            return core;
+        }
+
+        public NefsFileSys GetNefsFileSys()
+        {
+            if (fileSys == null)
+            {
+                fileSys = NefsFileSys.GetNefsFileSys();
+            }
+            return fileSys;
+        }
     }
 }
